708d0aa7bc6300051dbcbb4b36f4c60d6014c857
Balram1808 <145094339+Balram1808@users.noreply.github.com>
2023-10-13 15:58:29 +0530

    Add files via upload

---
+++ ChangeCase.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ChangeCase
+{
+	public static void main(String[] args) {
+	    //create a string
+	   	System.out.println("Enter a String");
+	   	Scanner sc = new Scanner(System.in);
+	   	String s = sc.nextLine();
+	   	System.out.println("Enter choice 0 for Upercase 1 for Lowercase");
+	   	int i = sc.nextInt();
+	   	switch(i){
+	   	    case 0:{
+	   	        System.out.println(s.toUpperCase());
+	   	        break;
+	   	    }
+	   	    case 1:{
+	   	         System.out.println(s.toLowerCase());
+	   	        break;
+	   	    }
+	   	    default:{
+	   	        System.out.println("Invalid Choice");
+	   	    }
+	   	        
+	   	}
+	}
+}
+
