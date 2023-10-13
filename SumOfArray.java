6e6d75817891479855c882549a359b26bb7d5733
Balram1808 <145094339+Balram1808@users.noreply.github.com>
2023-10-13 16:24:06 +0530

    Add files via upload

---
+++ SumOfArray.java
@@ -0,0 +1,15 @@
+public class SumOfArray
+{
+	public static void main(String[] args) {
+	    
+		int arr[] = {1,2,3,4,5,6,7,8,9,10} ;
+    int sum =0;
+    double avg;
+		for(int i:arr){
+		    sum +=i;
+		}
+		System.out.println("Sum = "+ sum);
+		avg = sum/arr.length;
+		System.out.println("Average = "+ avg);
+	}
+}
