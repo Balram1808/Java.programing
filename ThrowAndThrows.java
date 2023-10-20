c40ef5c9ffd911092a2177716b0d71639f07f31e
Balram1808 <145094339+Balram1808@users.noreply.github.com>
2023-10-20 16:17:57 +0530

    Add files via upload

---
+++ ThrowAndThrows.java
@@ -0,0 +1,20 @@
+public class ThrowAndThrows
+{
+    public static void method() throws ArithmeticException{
+        
+        	System.out.println("inside the methode");
+        	throw new ArithmeticException("throwing ArithmeticException");
+            
+    }
+    
+	public static void main(String[] args) {
+	    try{
+	        method();
+	    }
+	    catch(ArithmeticException e)
+	    {
+	      	System.out.println("caught on main() methode");  
+	    }
+	    
+	   	}
+}
